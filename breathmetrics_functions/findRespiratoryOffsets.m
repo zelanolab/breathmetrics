734d71c424d599f10eb9600a2a2ebdbcc62d9fe0
Torben Noto <torben.noto@gmail.com>
2018-01-05 11:47:20 -0600

    fix off by one error in finding offsets

--- breathmetrics_functions/findRespiratoryOffsets.m
+++ breathmetrics_functions/findRespiratoryOffsets.m
@@ -9,18 +9,18 @@ exhaleOffsets = zeros(size(exhaleOnsets));
 % finding inhale offsets
 for bi = 1:length(exhaleOnsets)
     if isnan(inhalePauseOnsets(bi))
-        inhaleOffsets(1, bi) = exhaleOnsets(bi);
+        inhaleOffsets(1, bi) = exhaleOnsets(bi)-1;
     else
-        inhaleOffsets(1, bi) = inhalePauseOnsets(bi);
+        inhaleOffsets(1, bi) = inhalePauseOnsets(bi)-1;
     end
 end
 
 % finding exhale offsets
 for bi = 1:length(exhaleOnsets) - 1
     if isnan(exhalePauseOnsets(bi))
-        exhaleOffsets(1, bi) = inhaleOnsets(bi + 1);
+        exhaleOffsets(1, bi) = inhaleOnsets(bi + 1)-1;
     else
-        exhaleOffsets(1, bi) = exhalePauseOnsets(bi);
+        exhaleOffsets(1, bi) = exhalePauseOnsets(bi)-1;
     end
 end
 
@@ -33,10 +33,10 @@ avgExhaleLen = mean(exhaleOffsets(1, 1:end-1) - exhaleOnsets(1, 1:end-1));
 lowerLim = avgExhaleLen / 4;
 upperLim = avgExhaleLen * 1.75;
 if isempty(putativeExhaleOffset) || putativeExhaleOffset < lowerLim || ...
-        putativeExhaleOffset > upperLim
+        putativeExhaleOffset >= upperLim
     % end of exhale cannot be calculated
     exhaleOffsets(1,end) = nan;
 else
-    exhaleOffsets(1,end) = exhaleOnsets(1,end) + putativeExhaleOffset;
+    exhaleOffsets(1,end) = exhaleOnsets(1,end) + putativeExhaleOffset - 1;
 end
 
